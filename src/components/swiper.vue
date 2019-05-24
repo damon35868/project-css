02929fbe46d53669d32e934e9a49e81bbd18ecdb
DM <578328962@qq.com>
2019-05-24 20:03:07 +0800

    1

--- src/components/swiper.vue
+++ src/components/swiper.vue
@@ -48,7 +48,7 @@ export default {
 <style scoped>
   .swiper-container {
     height: auto;
-    max-height: 650px;
+    max-height: 680px;
   }  
   .swiper-pagination>>>.swiper-pagination-bullet{
     opacity: 1;
